4a6048fb84df7ffa11a4ac12f3dacb062877b7f8
Makmadon <57114551+Makmadon@users.noreply.github.com>
2023-05-12 07:26:09 -0400

    Update Compatibles.c
    Se agrergaron los comentarios en las funciones

--- Compatibles.c
+++ Compatibles.c
@@ -1,14 +1,21 @@
 #include "Listas.h"
 
-
+//Se verifica que el par de cruces C1 y C2 sea un par que pueden cruzar al mismo tiempo
 int Posible(Cruces *C1, Cruces *C2,C_incompatible *L){
+//Si la lista de Cruces incompatibles es vacia entonces todos los pares de cruces son permitidos
     if(L==NULL)
         return 1;
+//Si el segundo par de cruce es nulo entonces no quedan mas cruces que comparar
     if(C2==NULL)
         return 1;
+//Se compara el primer par de cruces con ambos pares de lla lista de incompatibles
     if(!strcmp(C1->Cruce,L->Par1) || !strcmp(C1->Cruce,L->Par2))
+//Si el primer par se encuentra, se verifica eque el segundo tambien lo este en alguno de los dos pares
+//en caso de estar ambos significa que el cruce es invalido, se retorna falso.
         if(!strcmp(C2->Cruce,L->Par1) || !strcmp(C2->Cruce,L->Par2))
             return 0;
+//Para mayor eficiencia se hace recursion de cola pero con el siguiente giro incompatible
+//de esta forma se verifica toda la lista de giros incompatibles
     return (Posible(C1,C2,L->next));
 }
 
